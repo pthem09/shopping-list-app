f5fa6715af899fea8349ebf6dbd52f12ba1b04d5
Paul Them <->
2024-07-27 17:23:55 -0400

    fixing dependencies

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import ShoppingList from './Components/ShoppingList/ShoppingList';
 export default function App() {
   const [shoppingList, setShoppingList] = useState([]);
 
-  function loadData() {
-    fetch('https://hn7jn8-8080.csb.app/api/list')
+  const loadData = () => {
+    fetch("https://hn7jn8-8080.csb.app/api/list")
       .then(x=> x.json())
       .then(response => {
         setShoppingList(response);
@@ -24,6 +24,7 @@ export default function App() {
       <main>
         <ShoppingForm />
         <ShoppingList />
+        <p>JSON.stringify(shoppingList)</p>
       </main>
 
     </div>
