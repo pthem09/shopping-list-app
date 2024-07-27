534d681063cc2294541916bb36cf167ca59cf99b
Paul Them <->
2024-07-27 17:15:02 -0400

    adding shopping form and list components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import ShoppingForm from './Components/ShoppingForm/ShoppingForm';
+import ShoppingList from './Components/ShoppingList/ShoppingList';
 
 export default function App() {
+  const [shoppingList, setShoppingList] = useState([]);
+
+  function loadData() {
+    fetch('https://hn7jn8-8080.csb.app/api/list')
+      .then(x=> x.json())
+      .then(response => {
+        setShoppingList(response);
+      });
+  }
+
   return (
     <div className="App">
       <header>
         <h1>Shopping List App</h1>
       </header>
+
+      <main>
+        <ShoppingForm />
+        <ShoppingList />
+      </main>
+
     </div>
   );
 }
